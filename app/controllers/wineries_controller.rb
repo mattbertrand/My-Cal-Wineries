228447bc1c0e29dc74329acf5d41fbccdb62a4f3
github username <github email address>
2021-02-02 11:02:15 -0800

    added show page for wineries

--- app/controllers/wineries_controller.rb
+++ app/controllers/wineries_controller.rb
@@ -1,5 +1,16 @@
 class WineriesController < ApplicationController
+    before_action :find_winery, only: [:show]
+    
     def index
         @wineries = Winery.all
     end
+
+    def show
+        
+    end
+
+    private
+        def find_winery
+            @winery = Winery.find_by_id(params[:id])
+        end
 end
