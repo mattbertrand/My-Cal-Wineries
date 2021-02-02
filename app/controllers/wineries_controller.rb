f51a956dd06fe4a7574d4259ebf9d7d827c49f45
github username <github email address>
2021-02-02 11:42:50 -0800

    added new page for wineries

--- app/controllers/wineries_controller.rb
+++ app/controllers/wineries_controller.rb
@@ -9,8 +9,25 @@ class WineriesController < ApplicationController
         
     end
 
+    def new
+        @winery = Winery.new
+    end
+
+    def create
+        @winery = Winery.new(winery_params)
+        if @winery.save
+            redirect_to wineries_path 
+        else
+            render :new
+        end
+    end
+
     private
         def find_winery
             @winery = Winery.find_by_id(params[:id])
+        end
+
+        def winery_params
+            params.require(:winery).permit(:name, :website, :phone)
         end
 end
--- app/models/winery.rb
+++ app/models/winery.rb
@@ -1,3 +1,3 @@
 class Winery < ApplicationRecord
-  
+  validates :name, presence: true
 end
