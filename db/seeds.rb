e6b11f42b6ac7ceaa8993b0b730319d9a0cb5132
github username <github email address>
2021-02-02 10:14:15 -0800

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Winery.create([
+    { name: "Francis Ford Coppola Winery", website: "www.francisfordcoppolawinery.com", phone: "(707) 857-1400" },
+    { name: "Alexander Valley Vineyards", website: "www.avvwine.com", phone: "(707) 431-0869" },
+    { name: "J. Lohr San Jose Wine Center", website: "www.jlohr.com", phone: "(408) 918-2160" },
+    { name: "Justin Vineyards & Winery", website: "www.justinwine.com", phone: "(805) 238-6932" },
+    { name: "Alpha Omega Winery", website: "www.AOWinery.com", phone: "(707) 963-9999" }
+])
+
