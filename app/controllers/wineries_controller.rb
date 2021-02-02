10bc2555624654903541b34683d3b507d10ccb4f
github username <github email address>
2021-02-02 11:57:36 -0800

    added edit page for wineries and delete method

--- app/controllers/wineries_controller.rb
+++ app/controllers/wineries_controller.rb
@@ -1,5 +1,5 @@
 class WineriesController < ApplicationController
-    before_action :find_winery, only: [:show]
+    before_action :find_winery, only: [:show, :edit, :update, :destroy]
     
     def index
         @wineries = Winery.all
@@ -18,8 +18,28 @@ class WineriesController < ApplicationController
         if @winery.save
             redirect_to wineries_path 
         else
+            flash.now[:error] = @winery.errors.full_messages
             render :new
         end
+    end
+
+    def edit
+
+    end
+
+    def update
+        if @winery.update(winery_params)
+            redirect_to winery_path(@winery)
+        else
+            flash.now[:error] = @winery.errors.full_messages
+            render :edit
+        end
+    end
+
+    def destroy
+        @winery.destroy
+        flash[:notice] = "#{@winery.name} was deleted."
+        redirect_to wineries_path
     end
 
     private
