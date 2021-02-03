d9aac4a537b2ab1e779d62fb4c52448ccb7dc026
github username <github email address>
2021-02-03 11:25:44 -0800

    added partial errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :regions
   root to: 'static#home'
   
-  resources :wineries
+  
   devise_for :users
+  resources :wineries
+  resources :regions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210202205156_aad_region_id_to_wineries.rb
@@ -0,0 +1,4 @@
+class AadRegionIdToWineries < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
