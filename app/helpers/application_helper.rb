c17d37ee544c3b7ce4f30c3a57231cae657fbbda
github username <github email address>
2021-02-03 15:56:56 -0800

    added nav bar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+    def render_nav_links
+        content_tag("ul", class: "ight hide-on-med-and-down") do
+        if true
+                content_tag("li") do
+                    link_to("Discover Wineries", wineries_path)
+                end +
+                content_tag("li") do
+                    link_to("Add Winery", new_winery_path)
+                end +
+                content_tag("li") do
+                    link_to("Logout", "#")
+                end
+        else
+                content_tag("li") do
+                    link_to("Login", "#")
+                end +
+                content_tag("li") do
+                    link_to("Signup", "#")
+                end
+            end
+        end
+    end
 end
