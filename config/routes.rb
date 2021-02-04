3b71ba6591c322b230e6c2c916e3efb75239e15c
github username <github email address>
2021-02-03 16:40:17 -0800

    added nested ressource for region and wineries

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: 'static#home'
   
   
-  resources :wineries
-  resources :regions
   devise_for :users
+  resources :wineries
+  resources :regions do
+    resources :wineries, only: [:new, :create, :index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
