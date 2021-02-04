5fe8e92788ffd2880085af50e5f207262a6c2b65
github username <github email address>
2021-02-03 18:41:20 -0800

    added index page for region

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -1,5 +1,13 @@
 class RegionsController < ApplicationController
     
+
+    def index
+        @regions = Region.all
+    end
+
+    def show
+        @region = Region.find_by_id(params[:id])
+    end
     
     def new
         @region = Region.new
